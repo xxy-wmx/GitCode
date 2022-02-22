2c2227e0b6844e7691a06bb384b3899edf57641e
xiangyu.xu <xiangyu.xu@xquant.com>
2022-02-22 17:04:06 +0800

    First Commit

---
+++ src/main/java/com/xxy/git/GitTest.java
@@ -0,0 +1,8 @@
+package com.xxy.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Word！！！！");
+        System.out.println("TTTTTTTT");
+    }
+}
---
+++ src/main/java/com/xxy/git/Test.java
@@ -0,0 +1,7 @@
+package com.xxy.git;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello  word ");
+    }
+}
