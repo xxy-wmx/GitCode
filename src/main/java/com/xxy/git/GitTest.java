35a7ae2a606fde7c0f8a42ddd0ecf0441b85da28
xiangyu.xu <xiagnyu.xu@xquant.com>
2022-02-22 20:06:36 +0800

    push commit

--- src/main/java/com/xxy/git/GitTest.java
+++ src/main/java/com/xxy/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("444444");
         System.out.println("master test!!");
         System.out.println("hot-fix test");
+        System.out.println("push commit");
     }
 }
