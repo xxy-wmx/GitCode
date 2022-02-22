187c0b2071f57a4c34c3bfe936c5ae12d3db18fc
xiangyu.xu <xiangyu.xu@xquant.com>
2022-02-22 17:07:23 +0800

    Second Commit

--- src/main/java/com/xxy/git/GitTest.java
+++ src/main/java/com/xxy/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Word！！！！");
         System.out.println("TTTTTTTT");
+        System.out.println("TTTTTTTT");
+        System.out.println("TTTTTTTT");
     }
 }
